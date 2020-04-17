674ac40d42b0d5e243349e800be5cc051aaba4c9
Sébastien Murgey <sebastien.murgey@gmail.com>
2020-04-17 13:30:53 +0200

    Spring Initializr initial commit
    Signed-off-by: Sébastien MURGEY <sebastien@murgey.net>
    Signed-off-by: Sébastien MURGEY <sebastien.murgey@gmail.com>

---
+++ src/main/java/io/github/murgeyseb/ramuh/grid_overlay_provider/ProjectRamuhGridOverlayProviderApplication.java
@@ -0,0 +1,13 @@
+package io.github.murgeyseb.ramuh.grid_overlay_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRamuhGridOverlayProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRamuhGridOverlayProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/murgeyseb/ramuh/grid_overlay_provider/ProjectRamuhGridOverlayProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.murgeyseb.ramuh.grid_overlay_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRamuhGridOverlayProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
